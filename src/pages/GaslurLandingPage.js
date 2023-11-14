823230b4803943e59471a2169627be8a33b2deb5
ameeralimahar <ameeralimahar>
2023-11-14 07:13:21 +0000

    First Commit from Locofy Builder

---
+++ src/pages/GaslurLandingPage.js
@@ -0,0 +1,386 @@
+import "./GaslurLandingPage.css";
+
+const GaslurLandingPage = () => {
+  return (
+    <div className="gaslur-landing-page">
+      <div className="gaslur-landing-page-child" />
+      <div className="gaslur-landing-page-item" />
+      <div className="gaslur-landing-page-inner" />
+      <div className="discover-collect-and">
+        Discover, collect, and charity in extraordinary NFT marketplace
+      </div>
+      <div className="in-aenean-posuere">
+        In aenean posuere lorem risus nec. Tempor tincidunt aenean purus purus
+        vestibulum nibh mi venenatis
+      </div>
+      <div className="explore-wrapper">
+        <div className="explore">Explore</div>
+      </div>
+      <div className="create-wrapper">
+        <div className="explore">Create</div>
+      </div>
+      <div className="sign-in-parent">
+        <div className="sign-in">sign in</div>
+        <div className="create-parent">
+          <div className="create1">create</div>
+          <div className="group-child" />
+        </div>
+      </div>
+      <div className="load-more-parent">
+        <div className="load-more">Load more</div>
+        <div className="group-item" />
+      </div>
+      <img className="heart-24-icon" alt="" src="/heart24.svg" />
+      <div className="laura">Laura</div>
+      <div className="we-are-here">WE ARE HERE</div>
+      <div className="weth">0.21 Weth</div>
+      <div className="lorem-ipsum-parent">
+        <div className="lorem-ipsum">Lorem Ipsum</div>
+        <div className="weth1">1.20 Weth</div>
+      </div>
+      <div className="lorem-ipsum-group">
+        <div className="lorem-ipsum">Lorem Ipsum</div>
+        <div className="weth1">1.20 Weth</div>
+      </div>
+      <div className="lorem-ipsum-container">
+        <div className="lorem-ipsum">Lorem Ipsum</div>
+        <div className="weth1">1.20 Weth</div>
+      </div>
+      <div className="dolor-sit-amet-parent">
+        <div className="lorem-ipsum">Dolor sit amet</div>
+        <div className="weth1">0.21 Weth</div>
+      </div>
+      <div className="dolor-sit-amet-group">
+        <div className="lorem-ipsum">Dolor sit amet</div>
+        <div className="weth1">0.21 Weth</div>
+      </div>
+      <div className="dolor-sit-amet-container">
+        <div className="lorem-ipsum">Dolor sit amet</div>
+        <div className="weth1">0.21 Weth</div>
+      </div>
+      <div className="dignissim-curabitu-parent">
+        <div className="lorem-ipsum">Dignissim curabitu..</div>
+        <div className="weth1">0.01 Weth</div>
+      </div>
+      <div className="dignissim-curabitu-group">
+        <div className="lorem-ipsum">Dignissim curabitu..</div>
+        <div className="weth1">0.01 Weth</div>
+      </div>
+      <div className="dignissim-curabitu-container">
+        <div className="lorem-ipsum">Dignissim curabitu..</div>
+        <div className="weth1">0.01 Weth</div>
+      </div>
+      <div className="amet-pellentesq-parent">
+        <div className="lorem-ipsum">Amet pellentesq..</div>
+        <div className="weth1">0.55 Weth</div>
+      </div>
+      <div className="amet-pellentesq-group">
+        <div className="lorem-ipsum">Amet pellentesq..</div>
+        <div className="weth1">0.55 Weth</div>
+      </div>
+      <div className="amet-pellentesq-container">
+        <div className="lorem-ipsum">Amet pellentesq..</div>
+        <div className="weth1">0.55 Weth</div>
+      </div>
+      <img className="group-icon" alt="" src="/group-1248.svg" />
+      <div className="div">25</div>
+      <div className="hot-auctions">Hot auctions</div>
+      <img
+        className="unsplashrujm3dbxcqw-icon"
+        alt=""
+        src="/unsplashrujm3dbxcqw@2x.png"
+      />
+      <img
+        className="unsplashlki74jj7h-u-icon"
+        alt=""
+        src="/unsplashlki74jj7hu@2x.png"
+      />
+      <img
+        className="unsplashtryl7jyath0-icon"
+        alt=""
+        src="/unsplashtryl7jyath0@2x.png"
+      />
+      <img
+        className="unsplash9xngoipxceo-icon"
+        alt=""
+        src="/unsplash9xngoipxceo@2x.png"
+      />
+      <img
+        className="unsplashud5drkd4h6w-icon"
+        alt=""
+        src="/unsplashud5drkd4h6w@2x.png"
+      />
+      <img
+        className="unsplashfg7j6nnebbc-icon"
+        alt=""
+        src="/unsplashfg7j6nnebbc@2x.png"
+      />
+      <img
+        className="unsplashwn9du73b8-s-icon"
+        alt=""
+        src="/unsplashwn9du73b8-s@2x.png"
+      />
+      <img
+        className="unsplashhos3dzgphzw-icon"
+        alt=""
+        src="/unsplashhos3dzgphzw@2x.png"
+      />
+      <img
+        className="unsplashbu8texhspcy-icon"
+        alt=""
+        src="/unsplashbu8texhspcy@2x.png"
+      />
+      <img
+        className="unsplash7pqszt9kiey-icon"
+        alt=""
+        src="/unsplash7pqszt9kiey@2x.png"
+      />
+      <img
+        className="unsplashcrs2vlkse98-icon"
+        alt=""
+        src="/unsplashcrs2vlkse98@2x.png"
+      />
+      <img
+        className="unsplash9adeedyb2yk-icon"
+        alt=""
+        src="/unsplash9adeedyb2yk@2x.png"
+      />
+      <div className="view-all-parent">
+        <div className="view-all">View all</div>
+        <img className="arrow-right-24-icon" alt="" src="/arrowright24.svg" />
+      </div>
+      <div className="home-parent">
+        <b className="home">Home</b>
+        <div className="my-profile">My profile</div>
+        <div className="activity">activity</div>
+        <div className="how-it-works">how it works</div>
+        <div className="group-inner" />
+      </div>
+      <img className="line-icon" alt="" src="/line-11.svg" />
+      <img className="gaslur-landing-page-child1" alt="" src="/line-11.svg" />
+      <img className="gaslur-landing-page-child2" alt="" src="/line-11.svg" />
+      <img className="gaslur-landing-page-child3" alt="" src="/line-11.svg" />
+      <img className="gaslur-landing-page-child4" alt="" src="/line-11.svg" />
+      <img className="gaslur-landing-page-child5" alt="" src="/line-11.svg" />
+      <img className="gaslur-landing-page-child6" alt="" src="/line-11.svg" />
+      <img className="gaslur-landing-page-child7" alt="" src="/line-11.svg" />
+      <img className="gaslur-landing-page-child8" alt="" src="/line-11.svg" />
+      <img className="gaslur-landing-page-child9" alt="" src="/line-11.svg" />
+      <img className="gaslur-landing-page-child10" alt="" src="/line-11.svg" />
+      <img className="gaslur-landing-page-child11" alt="" src="/line-11.svg" />
+      <div className="ends-in-013445-parent">
+        <div className="ends-in-013445-container">
+          <span>Ends in</span>
+          <span className="span"> 01.34.45</span>
+        </div>
+        <div className="bid-parent">
+          <div className="bid">Bid</div>
+          <div className="rectangle-div" />
+        </div>
+      </div>
+      <div className="ends-in-013445-group">
+        <div className="ends-in-013445-container">
+          <span>Ends in</span>
+          <span className="span"> 01.34.45</span>
+        </div>
+        <div className="bid-parent">
+          <div className="bid">Buy</div>
+          <div className="rectangle-div" />
+        </div>
+      </div>
+      <div className="group-div">
+        <div className="ends-in-013445-container">
+          <span>Ends in</span>
+          <span className="span"> 01.34.45</span>
+        </div>
+        <div className="bid-parent">
+          <div className="bid">Bid</div>
+          <div className="rectangle-div" />
+        </div>
+      </div>
+      <div className="ends-in-013445-parent1">
+        <div className="ends-in-013445-container3">
+          <span>Ends in</span>
+          <span className="span"> 01.34.45</span>
+        </div>
+        <div className="bid-parent">
+          <div className="bid">Bid</div>
+          <div className="rectangle-div" />
+        </div>
+      </div>
+      <div className="ends-in-013445-parent2">
+        <div className="ends-in-013445-container3">
+          <span>Ends in</span>
+          <span className="span"> 01.34.45</span>
+        </div>
+        <div className="bid-parent">
+          <div className="bid">Buy</div>
+          <div className="rectangle-div" />
+        </div>
+      </div>
+      <div className="ends-in-013445-parent3">
+        <div className="ends-in-013445-container3">
+          <span>Ends in</span>
+          <span className="span"> 01.34.45</span>
+        </div>
+        <div className="bid-parent">
+          <div className="bid">Bid</div>
+          <div className="rectangle-div" />
+        </div>
+      </div>
+      <div className="ends-in-564445-parent">
+        <div className="ends-in-013445-container">
+          <span>Ends in</span>
+          <span className="span"> 56.44.45</span>
+        </div>
+        <div className="bid-parent">
+          <div className="bid">Bid</div>
+          <div className="rectangle-div" />
+        </div>
+      </div>
+      <div className="ends-in-564445-group">
+        <div className="ends-in-013445-container">
+          <span>Ends in</span>
+          <span className="span"> 56.44.45</span>
+        </div>
+        <div className="bid-parent">
+          <div className="bid">Buy</div>
+          <div className="rectangle-div" />
+        </div>
+      </div>
+      <div className="ends-in-564445-parent1">
+        <div className="ends-in-013445-container">
+          <span>Ends in</span>
+          <span className="span"> 56.44.45</span>
+        </div>
+        <div className="bid-parent">
+          <div className="bid">Bid</div>
+          <div className="rectangle-div" />
+        </div>
+      </div>
+      <div className="ends-in-013445-parent4">
+        <div className="ends-in-013445-container">
+          <span>Ends in</span>
+          <span className="span"> 01.34.45</span>
+        </div>
+        <div className="bid-parent4">
+          <div className="bid">Bid</div>
+          <div className="rectangle-div" />
+        </div>
+      </div>
+      <div className="ends-in-013445-parent5">
+        <div className="ends-in-013445-container">
+          <span>Ends in</span>
+          <span className="span"> 01.34.45</span>
+        </div>
+        <div className="bid-parent4">
+          <div className="bid">Buy</div>
+          <div className="rectangle-div" />
+        </div>
+      </div>
+      <div className="ends-in-013445-parent6">
+        <div className="ends-in-013445-container">
+          <span>Ends in</span>
+          <span className="span"> 01.34.45</span>
+        </div>
+        <div className="bid-parent4">
+          <div className="bid">Bid</div>
+          <div className="rectangle-div" />
+        </div>
+      </div>
+      <div className="group-parent">
+        <div className="group-container">
+          <div className="company-parent">
+            <div className="company">Company</div>
+            <div className="about-us-parent">
+              <div className="about-us">About Us</div>
+              <div className="services">Services</div>
+              <div className="portfolio">Portfolio</div>
+            </div>
+          </div>
+          <div className="marketplace-parent">
+            <div className="marketplace">Marketplace</div>
+            <div className="home-group">
+              <div className="home1">Home</div>
+              <div className="activity1">Activity</div>
+              <div className="discover">Discover</div>
+              <div className="learn-more">Learn more</div>
+            </div>
+          </div>
+          <div className="contact-parent">
+            <div className="contact">Contact</div>
+            <div className="facebook-parent">
+              <div className="facebook">Facebook</div>
+              <div className="instagram">Instagram</div>
+              <div className="twitter">Twitter</div>
+              <div className="email">Email</div>
+            </div>
+          </div>
+          <div className="etiam-et-id">
+            Etiam et id tincidunt faucibus mollis a sociis pretium fermentum
+            quis magna faucibus lacus.
+          </div>
+        </div>
+        <div className="copyright-2021-gaslur-parent">
+          <div className="copyright-2021-gaslur">Copyright 2021 Gaslur</div>
+          <div className="line-div" />
+          <div className="instagram-parent">
+            <img className="instagram-icon" alt="" src="/instagram@2x.png" />
+            <img className="facebook-icon" alt="" src="/facebook@2x.png" />
+            <img
+              className="twitter-squared-icon"
+              alt=""
+              src="/twitter-squared@2x.png"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="discover-parent">
+        <div className="discover1">Discover</div>
+        <div className="rectangle-parent">
+          <div className="group-child12" />
+          <div className="filter-parent">
+            <div className="filter">Filter</div>
+            <img
+              className="iconoutlinefilter"
+              alt=""
+              src="/iconoutlinefilter.svg"
+            />
+          </div>
+        </div>
+        <div className="cheapest-parent">
+          <div className="cheapest">Cheapest</div>
+          <img
+            className="iconoutlinecheveron-down"
+            alt=""
+            src="/iconoutlinecheverondown.svg"
+          />
+        </div>
+        <div className="newest-parent">
+          <div className="newest">Newest</div>
+          <img
+            className="iconoutlinecheveron-down1"
+            alt=""
+            src="/iconoutlinecheverondown1.svg"
+          />
+        </div>
+        <div className="category-parent">
+          <div className="category">Category</div>
+          <img
+            className="iconoutlinesort-ascending"
+            alt=""
+            src="/iconoutlinesortascending.svg"
+          />
+        </div>
+      </div>
+      <img
+        className="unsplashog44d93injk-icon"
+        alt=""
+        src="/unsplashog44d93injk@2x.png"
+      />
+      <b className="fastech">FasTech</b>
+    </div>
+  );
+};
+
+export default GaslurLandingPage;
